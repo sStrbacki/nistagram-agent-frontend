528967427f3e5bcc24da50f719ce98428ec30a8d
Nikola <nikola.gudelj@uns.ac.rs>
2021-07-03 02:10:49 +0200

    Refactor: Shipping code removed
    Too complex, needs to be rewritten from scratch #spaghetti

--- src/store/shopping-cart/index.js
+++ src/store/shopping-cart/index.js
@@ -1,5 +1,3 @@
-import api from '../../api/index';
-import axios from 'axios';
 import {getCartItems, removeFromCart} from "@/services/cartService";
 import {errorMessage, successMessage} from "@/helpers/notfications";
 import {deleteById} from "@/services/general";
@@ -9,66 +7,26 @@ export default {
         cart: {
             items: [],
         },
-        shippingAddress: {
-            address: null,
-            city: null,
-            state: null,
-            zipCode: null
-        }
     },
     getters: {
-        shoppingCartItems(state) {
+        cartItems(state) {
             return state.cart.items;
         },
-        fullShippingAddress(state) {
-            return state.cart.shippingAddress;
-        }
     },
     mutations: {
-        setCartItems(state, value) {
+        cartItems(state, value) {
             state.cart.items = value;
         },
         removeCartItemById(state, itemId) {
             deleteById(state.cart.items, itemId);
         },
-        resetItems(state) {
-            state.cart.items = [];
-        },
-        updateAddress(state, value) {
-            state.shippingAddress.address = value;
-        },
-        updateCity(state, value) {
-            state.shippingAddress.city = value;
-        },
-        updateState(state, value) {
-            state.shippingAddress.state = value;
-        },
-        updateZipCode(state, value) {
-            state.shippingAddress.zipCode = value;
-        },
-        resetShippingAddress(state) {
-            state.shippingAddress.address = null;
-            state.shippingAddress.city = null;
-            state.shippingAddress.state = null;
-            state.shippingAddress.zipCode = null;
-        },
-
     },
     actions: {
         fetchShoppingCart(context) {
             getCartItems()
             .then((response) => {
-                context.commit('setCartItems', response.data);
+                context.commit('cartItems', response.data);
             })
-        },
-        placeOrder(context) {
-            axios.post(api.invoices.root, context.getters.fullShippingAddress)
-            .then(() => {
-                successMessage('Order placed')
-                context.commit('resetItems');
-                context.commit('resetShippingAddress');
-            })
-            .catch(error => errorMessage(error.response.data));
         },
         removeFromShoppingCart(context, shoppingCartItemId) {
             removeFromCart(shoppingCartItemId)
@@ -77,7 +35,6 @@ export default {
                 successMessage('Removed from cart.');
             })
             .catch(err => {
-                console.log(err);
                 errorMessage(err.response.data);
             });
         }
--- src/views/user/ShoppingCart.vue
+++ src/views/user/ShoppingCart.vue
@@ -1,123 +1,40 @@
 <template>
   <div>
     <div class="d-flex flex-wrap justify-content-between items-wrap">
-      <b-col cols="4" v-for="item in items" :key="item.id">
+      <b-col cols="4" v-for="cartItem in cartItems" :key="cartItem.id">
         <div class="item mb-5 mr-4">
           <b-col>
-            <catalog-item :item="item.product" :cartItem="toCartItem(item)"/>
+            <catalog-item :item="cartItem.product" :cartItem="trimProduct(cartItem)"/>
           </b-col>
         </div>
       </b-col>
-    </div>
-    <b-button v-if="!showShipping" variant="success" @click="showShipping = true">Checkout</b-button>
-    <div v-else>
-      <b-row class="mb-3" cols="3" align-h="center">
-        <b-col>
-          <b-form-input :state="addressValid" v-model="address" placeholder="Address"></b-form-input>
-        </b-col>
-      </b-row>
-      <b-row class="mb-3" cols="3" align-h="center">
-        <b-col>
-          <b-form-input :state="cityValid" v-model="city" placeholder="City"></b-form-input>
-        </b-col>
-      </b-row>
-      <b-row class="mb-3" cols="6" align-h="center">
-        <b-col>
-          <b-form-input :state="stateValid" v-model="state" placeholder="State"></b-form-input>
-        </b-col>
-        <b-col>
-          <b-form-input :state="zipCodeValid" v-model="zipCode" placeholder="Zip Code"></b-form-input>
-        </b-col>
-      </b-row>
-      <b-button :disabled="!formValid" variant="success" @click="placeOrder">Confirm Order</b-button>
     </div>
   </div>
 </template>
 
 <script>
 import CatalogItem from '../../components/catalog/CatalogItem'
+import {deepCopy} from "@/services/general";
 
 export default {
   name: 'ShoppingCart',
   components: {CatalogItem},
   computed: {
-    items: {
+    cartItems: {
       get() {
-        return this.$store.getters.shoppingCartItems;
+        return this.$store.getters.cartItems;
       }
     },
-    address: {
-      get() {
-        return this.$store.state.shoppingCart.shippingAddress.address;
-      },
-      set(value) {
-        this.$store.commit('updateAddress', value);
-      }
-    },
-    city: {
-      get() {
-        return this.$store.state.shoppingCart.shippingAddress.city;
-      },
-      set(value) {
-        this.$store.commit('updateCity', value);
-      }
-    },
-    state: {
-      get() {
-        return this.$store.state.shoppingCart.shippingAddress.state;
-      },
-      set(value) {
-        this.$store.commit('updateState', value);
-      }
-    },
-    zipCode: {
-      get() {
-        return this.$store.state.shoppingCart.shippingAddress.zipCode;
-      },
-      set(value) {
-        this.$store.commit('updateZipCode', value);
-      }
-    },
-    addressValid() {
-      return this.notEmpty(this.address);
-    },
-    cityValid() {
-      return this.notEmpty(this.city);
-    },
-    stateValid() {
-      return this.notEmpty(this.state);
-    },
-    zipCodeValid() {
-      return this.notEmpty(this.zipCode) && /^\d+$/.test(this.zipCode);
-    },
-    formValid() {
-      return this.zipCodeValid && this.stateValid && this.stateValid && this.cityValid && this.addressValid;
-    }
-  },
-  data() {
-    return {
-      showShipping: false
-    };
   },
   mounted() {
     this.$store.dispatch('fetchShoppingCart');
   },
   methods: {
-    placeOrder() {
-      this.$store.dispatch('placeOrder');
-      this.showShipping = false;
+    trimProduct(item) {
+      const cartItem = deepCopy(item);
+      cartItem.product = undefined;
+      return cartItem;
     },
-    toCartItem(item) {
-      return {
-        id: item.id,
-        quantity: item.quantity
-      }
-    },
-    notEmpty(field) {
-      if (field === '') return false;
-      if (!field) return null;
-      return true;
-    }
   }
 }
 </script>
